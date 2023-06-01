32a54c915faca6ff0bd6b504914c9248d001c27b
SamvPy <you@example.com>
2023-06-01 11:07:24 +0200

    cleaning

--- count_spectra.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-gzip -dk -S .gzip -r *
-grep -r --include \*.mgf -c "^BEGIN IONS"  >>output.txt
-find . -name "*.mgf" -type f -delete
-find . -name "*.gzip" -type f -delete
-find . -name "*.gz" -type f -delete
